313165a10df36e5f3b5f3d4ffdf5d4173db32540
Дмитрий <dmpul@mail.ru>
2024-05-19 21:09:43 +0300

    Add files via upload

---
+++ HomeWork4.py
@@ -0,0 +1,11 @@
+# immutable_var= 1,2,3,4,5,6,7, True, 'Age'
+# immutable_var[1]=300
+# print(immutable_var)
+# 4. Создание изменяемых структур данных:
+mutable_list=([1,2,3,4,5,6,7, True, 'Age'])
+print(mutable_list)
+mutable_list[0]=6
+mutable_list[5]='Live is live'
+mutable_list[8]='Odin'
+mutable_list[3]= 65
+print(mutable_list) #-Выведите на экран измененный список mutable_list.
